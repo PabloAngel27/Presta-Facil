32bbe8d7c2a82336eb95deaa7aff631e4e787576
PabloAngel27 <PabloAngelLamarche@gmail.com>
2017-05-03 13:42:32 -0400

    Edicion De FORM5

--- Form5.Designer.vb
+++ Form5.Designer.vb
@@ -27,18 +27,20 @@ Partial Class Prestamos
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.NPestamo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Pestamo = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Cliente = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.FAmonestacion = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.BInicial = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.BAnterior = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.BActual = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Amonestacion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Inicial = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Anterior = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Actual = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(613, 116)
+        Me.Button4.Location = New System.Drawing.Point(31, 106)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 23)
         Me.Button4.TabIndex = 28
@@ -47,7 +49,7 @@ Partial Class Prestamos
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(453, 116)
+        Me.Button3.Location = New System.Drawing.Point(31, 77)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 23)
         Me.Button3.TabIndex = 27
@@ -56,7 +58,7 @@ Partial Class Prestamos
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(256, 116)
+        Me.Button2.Location = New System.Drawing.Point(31, 48)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 26
@@ -65,7 +67,7 @@ Partial Class Prestamos
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(65, 116)
+        Me.Button1.Location = New System.Drawing.Point(31, 19)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 25
@@ -75,54 +77,66 @@ Partial Class Prestamos
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NPestamo, Me.Cliente, Me.FAmonestacion, Me.BInicial, Me.BAnterior, Me.BActual})
-        Me.DataGridView1.Location = New System.Drawing.Point(27, 145)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Pestamo, Me.Cliente, Me.Amonestacion, Me.Inicial, Me.Anterior, Me.Actual})
+        Me.DataGridView1.Location = New System.Drawing.Point(27, 234)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(730, 299)
+        Me.DataGridView1.Size = New System.Drawing.Size(730, 315)
         Me.DataGridView1.TabIndex = 24
         '
-        'NPestamo
+        'GroupBox2
         '
-        Me.NPestamo.HeaderText = "NPestamo"
-        Me.NPestamo.Name = "NPestamo"
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Location = New System.Drawing.Point(628, 17)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(139, 145)
+        Me.GroupBox2.TabIndex = 29
+        Me.GroupBox2.TabStop = False
+        '
+        'Pestamo
+        '
+        Me.Pestamo.HeaderText = "Pestamo"
+        Me.Pestamo.Name = "Pestamo"
         '
         'Cliente
         '
         Me.Cliente.HeaderText = "Cliente"
         Me.Cliente.Name = "Cliente"
+        Me.Cliente.Width = 175
         '
-        'FAmonestacion
+        'Amonestacion
         '
-        Me.FAmonestacion.HeaderText = "FAmonestacion"
-        Me.FAmonestacion.Name = "FAmonestacion"
+        Me.Amonestacion.HeaderText = "Amonestacion"
+        Me.Amonestacion.Name = "Amonestacion"
+        Me.Amonestacion.Width = 110
         '
-        'BInicial
+        'Inicial
         '
-        Me.BInicial.HeaderText = "BInicial"
-        Me.BInicial.Name = "BInicial"
+        Me.Inicial.HeaderText = "Inicial"
+        Me.Inicial.Name = "Inicial"
         '
-        'BAnterior
+        'Anterior
         '
-        Me.BAnterior.HeaderText = "BAnterior"
-        Me.BAnterior.Name = "BAnterior"
+        Me.Anterior.HeaderText = "Anterior"
+        Me.Anterior.Name = "Anterior"
         '
-        'BActual
+        'Actual
         '
-        Me.BActual.HeaderText = "BActual"
-        Me.BActual.Name = "BActual"
+        Me.Actual.HeaderText = "Actual"
+        Me.Actual.Name = "Actual"
         '
         'Prestamos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(784, 561)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Prestamos"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -131,10 +145,11 @@ Partial Class Prestamos
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents NPestamo As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Pestamo As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Cliente As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents FAmonestacion As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents BInicial As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents BAnterior As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents BActual As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Amonestacion As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Inicial As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Anterior As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Actual As System.Windows.Forms.DataGridViewTextBoxColumn
 End Class
