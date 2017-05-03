6027cbeaf5ebc57830282322003f6358a83cd120
pxp20 <petete.racinq@gmail.com>
2017-05-03 13:10:58 -0400

    form5
    ok

--- Form5.Designer.vb
+++ Form5.Designer.vb
@@ -22,28 +22,119 @@ Partial Class Prestamos
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.NPestamo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Cliente = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FAmonestacion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BInicial = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BAnterior = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BActual = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(613, 116)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 28
+        Me.Button4.Text = "Eliminar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(453, 116)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 27
+        Me.Button3.Text = "Modificar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(256, 116)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Agregar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(65, 116)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "Actualizar"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(24, 250)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NPestamo, Me.Cliente, Me.FAmonestacion, Me.BInicial, Me.BAnterior, Me.BActual})
+        Me.DataGridView1.Location = New System.Drawing.Point(27, 145)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(730, 299)
-        Me.DataGridView1.TabIndex = 0
+        Me.DataGridView1.TabIndex = 24
+        '
+        'NPestamo
+        '
+        Me.NPestamo.HeaderText = "NPestamo"
+        Me.NPestamo.Name = "NPestamo"
+        '
+        'Cliente
+        '
+        Me.Cliente.HeaderText = "Cliente"
+        Me.Cliente.Name = "Cliente"
+        '
+        'FAmonestacion
+        '
+        Me.FAmonestacion.HeaderText = "FAmonestacion"
+        Me.FAmonestacion.Name = "FAmonestacion"
+        '
+        'BInicial
+        '
+        Me.BInicial.HeaderText = "BInicial"
+        Me.BInicial.Name = "BInicial"
+        '
+        'BAnterior
+        '
+        Me.BAnterior.HeaderText = "BAnterior"
+        Me.BAnterior.Name = "BAnterior"
+        '
+        'BActual
+        '
+        Me.BActual.HeaderText = "BActual"
+        Me.BActual.Name = "BActual"
         '
         'Prestamos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Prestamos"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents NPestamo As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Cliente As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents FAmonestacion As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BInicial As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BAnterior As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BActual As System.Windows.Forms.DataGridViewTextBoxColumn
 End Class
